bf37894f93647054718e7e8b009decd20132cc6e
Thelast-name <miw45500@gmail.com>
2022-10-22 00:22:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+
+const file = require('./dataj.json') 
+
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', function (req, res) {
+    res.json(file)
+    
+})
+
+// fetch('dataj.json')
+// .then(response => response.json())
+// .then(json => console.log(json))
+
+
+
+app.listen(8000, () => {
+    console.log('Server runing')
+})
